1d57846493300604c9eb4a0e9e8b3df23a6e1b4c
Cody Soyland <codysoyland@gmail.com>
2010-08-09 19:32:13 -0500

    Fix setup.py. Tag for release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-README = read('README.md')
+README = read('README.rst')
 
 setup(
     name = "django-smart-load-tag",
